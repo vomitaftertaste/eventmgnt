50bb98b3e3931cf5bf5ccd4dff6896fbddbfd42a
Administrator <Administrator@ROM-LAB05>
2017-10-27 10:58:58 +0800

    add user login test

--- EventMgt/Event/LoginTest.cs
+++ EventMgt/Event/LoginTest.cs
@@ -2,12 +2,15 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-
 namespace Event
 {
     [TestFixture]
     class LoginTest
     {
-
+        [Test]
+        public void testWrongLogin()
+        {
+            Assert.AreEqual("User does not exist", User.login("user_wrong","pass"));
+        }
     }
 }
---
+++ EventMgt/Event/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System;
+
+namespace Event
+{
+    public static class User
+    {
+        public static string login(String username, String password)
+        {
+            if (username != "user")
+            {
+                return "User does not exist";
+            }
+            else
+            {
+                if (password == "password")
+                {
+                    return "Login successful";
+                }
+                else
+                {
+                    return "Wrong password";
+                }
+            }
+        }
+    }
+}
