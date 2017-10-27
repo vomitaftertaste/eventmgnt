3c07435596e51951baf3d34a50b97d8927d071b9
Administrator <Administrator@ROM-LAB04>
2017-10-27 11:00:21 +0800

    Fix course detail

--- EventMgt/Event/Course.cs
+++ EventMgt/Event/Course.cs
@@ -8,5 +8,15 @@ namespace Event
         public string Name { get; set; }
         public string Description { get; set; }
         public DateTime CourseDate { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            Course course = obj as Course;
+            if(ID == course.ID && Name == course.Name && Description == course.Description &&
+                CourseDate == course.CourseDate)
+                return true;
+
+            return false;
+        }
     }
 }
