e0a8feff857583a43186d7bac5d41ac86673e42e
Administrator <Administrator@ROM-LAB04>
2017-10-27 10:40:16 +0800

    Course Detail

---
+++ EventMgt/EventMgt/CourseRep.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EventMgt
+{
+    public class CourseRep
+    {
+        public Course GetCourseDetail(int id)
+        {
+            Course course = new Course();
+            course.ID = 1;
+            course.Name = "Professional Scrum Developer";
+            course.Description = "Scrum Programming";
+            course.CourseDate = new DateTime(2017, 10, 25);
+            return course;
+        }
+    }
+}
---
+++ EventMgt/EventMgt/CourseTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventMgt
+{
+    [TestFixture]
+    class CourseTest
+    {
+        [Test]
+        public void GetCourseDetail()
+        {
+            Course course = new Course();
+            course.ID = 1;
+            course.Name = "Professional Scrum Developer";
+            course.Description = "Scrum Programming";
+            course.CourseDate = new DateTime(2017, 10, 25);
+
+            CourseRep rep = new CourseRep();
+
+            Assert.Equals(course, rep.GetCourseDetail(1));
+        }
+    }
+}
